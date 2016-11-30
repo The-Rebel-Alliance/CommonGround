271ccea3c301bfe592ed68512ee3f74df65afb5d
JamesShabo <jamesshabo@Jamess-MacBook-Pro.local>
2016-11-30 15:32:38 -0800

    adding sidebar component

--- src/client/api/users.js
+++ src/client/api/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import store from 'store'
-import {browswerHistory} from 'react-router'
+import {browserHistory} from 'react-router'
 import api from 'lib/api'
 
 api.new('/')
@@ -9,7 +9,7 @@ api.setTokenPath('/login')
 export function createUser(obj) {
     return axios.post('/register', {username: obj.username, password: obj.password, first_name: obj.first_name, last_name: obj.last_name, avatar: obj.avatar, city: obj.city, state: obj.state, political_affiliation: obj.political_affiliation}).then(function(resp){
       console.log(resp)
-        browswerHistory.push("/") 
+        browserHistory.push("/") 
     })
   }
 
---
+++ src/client/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { browserHistory, Link } from 'react-router'
+import users from 'api/users'
+import 'assets/styles/sidebar.css'
+
+const Sidebar = React.createClass({
+    getInitialState: function(){
+      return(
+          <div id="sidebar">
+            <aside>
+              
+            </aside>
+          </div>
+      )
+    }
+})
