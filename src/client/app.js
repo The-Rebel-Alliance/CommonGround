fe75e0dd62c38addc27af0a0e8377f7ad87f3ca8
JamesShabo <jamesshabo@yahoo.com>
2016-12-02 10:46:45 -0800

    Added messaging component, still need tweaking abit

--- src/client/app.js
+++ src/client/app.js
@@ -2,15 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route, browserHistory } from 'react-router'
 
+//Style Normalize
 import 'normalize.scss/normalize.scss'
 
-
+//Components
 import Login from 'components/Login'
 import Register from 'components/Register'
+import MessagingContainer from 'components/MessagingContainer'
+import TextBox from 'components/TextBox'
 
 ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={Login} />
     <Route path="/register" component={Register} />
+    <Route path="/messaging" component={MessagingContainer} />
+    <Route path="/messaging" component={TextBox} />
   </Router>
 ), document.getElementById('app'))
