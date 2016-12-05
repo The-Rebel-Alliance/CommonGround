6dc2db0eefdc246bfa131dec3b27980cfb4487a0
JamesShabo <jamesshabo@yahoo.com>
2016-12-05 15:06:40 -0800

    styled message container some more 12/5

--- src/client/components/MessagingContainer.js
+++ src/client/components/MessagingContainer.js
@@ -8,15 +8,17 @@ const MessagingContainer = React.createClass({
       return(
           <div id="messagingContainer">
             <div id="messages">
+              <div className="message">
               <ul>
-                <div id="username"><h6>(James Shabo)</h6></div>
-                  <img src="http://orig02.deviantart.net/33e4/f/2010/188/6/a/twokinds_fanart___avatar_sized_by_turaiel.gif"></img><div id="time"><h6>(12:00am)</h6></div>
+                <div id="username"><h5>(James Shabo)</h5></div><div id="time"><h6>(12:00am)</h6></div>
+                  <img src="http://orig02.deviantart.net/33e4/f/2010/188/6/a/twokinds_fanart___avatar_sized_by_turaiel.gif"></img>
                   <li>:I'm Great! What are your views on (insert topic here)</li> <br />
                 </ul>
-          <div id="textboxContainer">
-            <form id="textBox">
-              <input type="text" name="textBox" id="inputBox"></input><button>Submit</button>
-            </form>
+                </div>
+            <div id="textboxContainer">
+              <form id="textBox">
+            <input type="text" name="textBox" id="inputBox"></input><button>Submit</button>
+              </form>
         </div>
       </div>
     </div>
