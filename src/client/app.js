e279f86f7f78ff1c1f597cb01385135cdabdd061
dustinrudy <drudyprop@gmail.com>
2016-12-06 10:42:40 -0800

    Fixed the styling conflicts and linked the search and fixed layout component

--- src/client/app.js
+++ src/client/app.js
@@ -8,13 +8,14 @@ import 'normalize.scss/normalize.scss'
 //Components
 import Login from 'components/Login'
 import Register from 'components/Register'
-import MessagingContainer from 'components/MessagingView'
-import TextBox from 'components/TextBox'
 import Dashboard from 'components/dashboardSearch'
 import ProfileView from 'components/ProfileView'
-import Drawer from 'components/Drawer'
 import ProfileEdit from 'components/ProfileEdit'
 import Profile from 'components/Profile'
+
+// Layout
+
+import Layout from 'layouts/Layout'
 
 
 
@@ -22,11 +23,11 @@ ReactDOM.render((
   <Router history={browserHistory}>
     <Route path="/" component={Login} />
     <Route path="/register" component={Register} />
-    <Route path="/messaging" component={MessagingContainer} />
-    <Route path="/dashboard" component={Dashboard} />
-    <Route path="/profile" component={Profile} />
-    <Route path="/profile/:id" component={ProfileView} />
-    <Route path="/editprofile" component={ProfileEdit} />
-    <Route path="/drawer" component={Drawer} />
+    <Route component={Layout}>
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+      <Route path="/profile/:id" component={ProfileView} />
+      <Route path="/editprofile" component={ProfileEdit} />
+    </Route>
   </Router>
 ), document.getElementById('app'))
--- src/client/components/Profile.js
+++ src/client/components/Profile.js
@@ -60,7 +60,7 @@ const CommonProfile = React.createClass({
         <div className="topics_container">
         {this.props.topics.map(item => {
           return (
-          <div className="indiv_topic_container">
+          <div key={item.id} className="indiv_topic_container">
             <h3 className="topic_header">{item.name}</h3>
             <p className="topic_stance">{item.stance}</p>
           </div>
