4686bb4a25dbe0482aefca46dcdca242cd264a99
Michael Sweeney <jfmike@gmail.com>
2016-12-06 15:02:06 -0800

    fixed socket on backend

--- src/server/video/chat.js
+++ src/server/video/chat.js
@@ -7,42 +7,21 @@ export default function(io, req) {
 
     console.log('socket connected')
     var room = ''
-    var username = ''
+
     socket.on('join', function(roomName){
       socket.join(roomName)
       room = roomName
     })
 
     socket.on('vid message', function(msg){
-      if (username === '') {
-        let token = req.cookies.token
-        console.log(token)
-
-        const sql = `
-          SELECT u.username 
-          FROM users u
-          JOIN tokens t ON t.user_id = u.user_id
-          WHERE t.token = ?
-        `
-
-        conn.query(sql, [token], function(err, results){
-          username = results[0].username
-
-          socket.emit('vid message', {
-            user: username,
-            message: msg
-          })
-        })
-      } else {
-        socket.emit('vid message', {
-          user:username,
-          message: msg
-        })
-      }  
+      socket.to(room).emit('vid message', {
+        user: username,
+        message: msg
+      })
     })
 
     socket.on('disconnect', function(){
-      socket.emit('disconnect', {
+      socket.to(room).emit('disconnect', {
         user: username
       })
     })
