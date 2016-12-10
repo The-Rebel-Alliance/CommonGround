00552a1bf8645b983dffb71aa6bf0fabcbf8f10b
Michael Sweeney <jfmike@gmail.com>
2016-12-10 15:38:14 -0800

    changed messages component to autoscroll

--- src/client/components/MessagingView.js
+++ src/client/components/MessagingView.js
@@ -6,7 +6,7 @@ import { getConvo } from 'api/getConvo'
 import { getMessageUsers } from 'api/getMessages'
 import { sendMsg } from 'api/sendMsg'
 import { toggleDrawer, closeDrawer } from 'api/toggleDrawer'
-
+import ChatWindow from 'components/ChatWindow'
 
 const MessagingView = React.createClass({
   getInitialState: function() {
@@ -53,44 +53,21 @@ const MessagingView = React.createClass({
   },
   render: function(){
     return(
-     
       <div id="messagingContainer">
-          <h4 className="messagesHeader">
-           <div className="profileLink">
-              <a onClick={this.goToProfile}>
-                  <img className="avatarLink"src={this.props.avatar}/>                        
-              </a>
-           </div>
-           <div className="videoLinkContainer">
-           <a onClick={this.generateRoom} >
+        <h4 className="messagesHeader">
+          <div className="profileLink">
+            <a onClick={this.goToProfile}>
+              <img className="avatarLink"src={this.props.avatar}/>                        
+            </a>
+          </div>
+          <div className="videoLinkContainer">
+            <a onClick={this.generateRoom} >
               Video Chat&nbsp;
-             <i id="videoLink" className="fa fa-video-camera" aria-hidden="true" >
-             </i>                  
-           </a>
-           </div>
-          </h4>
-         <div className="messages"> 
-          <ul>
-            {this.props.myconvo.map((chat,i) => {
-              if (/^\/v\//.test(chat.message)) {
-                return (
-                  <li className="you" id={'chat' + i} key={'chat' + i}>
-                   {chat.username}: Here's a link so we can video chat 
-                   <a className="videoLink" href={chat.message} target="_blank">{chat.message}
-                    <i className="fa fa-video-camera" aria-hidden="true"></i>
-                   </a>
-                  </li>
-                )
-              } else {
-                return (
-                  <li className={chat.from} id={'chat' + i} key={'chat' + i}>
-                    {chat.username}:   {chat.message} 
-                  </li>
-                )
-              }
-             })}
-          </ul>
-        </div>
+              <i id="videoLink" className="fa fa-video-camera" aria-hidden="true" ></i>
+            </a>
+          </div>
+        </h4>
+        <ChatWindow myconvo={this.props.myconvo} />
         <div id="textboxContainer">
           <form onSubmit={this.handleSubmit}id="textBox">
             <input className="chat_submit" value={this.state.message} onChange={this.update} type="text" name="textBox" id="message"/>
@@ -98,7 +75,6 @@ const MessagingView = React.createClass({
           </form>
         </div>
       </div>
-    
     )
   }
 })
