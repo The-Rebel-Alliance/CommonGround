7128028f8456f06d77db3cb364f78fe8fcde6c26
Michael Sweeney <jfmike@gmail.com>
2016-12-12 13:55:58 -0800

    added spectator count to socket

--- src/server/video/chat.js
+++ src/server/video/chat.js
@@ -1,7 +1,22 @@
 export default function(vid) {
+  var rooms = []
+
   vid.on('connection', function(socket){
-    console.log('socket connected')
     var room = ''
+    var particpants = []
+    var spectators = []
+
+    socket.on('particpant connect', function(user){
+      participants.push({
+        username:user, 
+        id:socket.id
+      })
+    })
+
+    socket.on('spectator connect', function(){
+      spectators.push(socket.id)
+      socket.emit('spec count', spectators.length)
+    })
 
     socket.on('join', function(roomName){
       socket.join(roomName)
@@ -14,6 +29,21 @@ export default function(vid) {
 
     socket.on('spec vote', function(vote){
       vid.to(room).emit('spec vote', vote)
+    })
+
+    socket.on('disconnect', function(){
+      spectators = spectators.filter(function(id){
+        return id !== socket.id
+      })
+      socket.emit('spec count', spectators.length)
+
+      particpants = particpants.filter(function(user){
+        return user.id !== socket.id
+      })
+
+      if (participants.length < 2) {
+        // change array of rooms
+      }
     })
   })
 }
