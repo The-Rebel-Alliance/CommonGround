484402c062a2ff57847f454b47b82f69af561bf5
JamesShabo <jamesshabo@yahoo.com>
2016-12-13 13:07:51 -0800

    fixed up/down vote

--- src/client/video/js/vidchat.js
+++ src/client/video/js/vidchat.js
@@ -9,8 +9,13 @@ var identity;
 var roomName = location.href.substr(location.href.lastIndexOf('/') + 1);
 var url = location.href.substr(0, location.href.indexOf(location.pathname))
 var path = location.pathname[1]
-var counter = 0
-
+// var counter = 0
+var counter = {
+    user1up:0,
+    user1down:0,
+    user2up:0,
+    user2down:0
+}
 
 var socket = io('/video').connect(url, {
   secure:true
@@ -35,65 +40,100 @@ $("#controls.s").append(
 
   "<div id='votingContainer'>\
     <div id='upvote1'>\
-      <button type='button' onclick='counter1()'>\
+      <button id='user1up' type='button' class='vote'>\
         <i id='interaction-agree2' class='fa fa-thumbs-up interaction-icons' aria-hidden='true'></i>\
       </button>\
     </div>\
     <div id='upCounter1'>\
-      <p id='counter1'></p>\
+      <p class='user1up'>##</p>\
     </div>\
     <div id='downvote1'>\
-      <button type='button' onclick='counter2()'>\
+      <button id='user1down' type='button' class='vote'>\
         <i id='interaction-disagree2' class='fa fa-thumbs-down interaction-icons' aria-hidden='true'></i>\
       </button>\
     </div> \
       <div id='downCounter1'>\
-        <p id='counter2'></p>\
+        <p class='user1down'>##</p>\
       </div>\
     <div id='upvote2'>\
-      <button type='button' onclick='counter3()'>\
+      <button id='user2up' type='button' class='vote'>\
         <i id='interaction-agree2' class='fa fa-thumbs-up interaction-icons' aria-hidden='true'></i>\
       </button>\
     </div>\
       <div id='upCounter2'>\
-        <p id='counter3'></p>\
+        <p class='user2up'>##</p>\
       </div>      \
     <div id='downvote2'>\
-      <button type='button' onclick='counter4()'>\
+      <button id='user2down' type='button' class='vote'>\
         <i id='interaction-disagree2' class='fa fa-thumbs-down interaction-icons' aria-hidden='true'></i>\
       </button>\
     </div>\
       <div id='downCounter2'>\
-        <p id='clicks4'></p>\
+        <p class='user2down'>##</p>\
       </div>\
   </div>")
 
-var clicks = 0; //This is for the UpVote/Downvote Counters
-var clicks2 = 0;
-var clicks3 = 0;
-var clicks4 = 0;
+$(".vote").on('click', function(e){
+  
+  var id = $(this).attr('id')
 
-function counter1() {
-  clicks += 1;
-  console.log(clicks)
-$('#counter1').add('#counter1')
-}
-function counter2() {
-  clicks2 += 1;
-  console.log(clicks2)
- $('#counter2')
-}
-function counter3() {
+  counter[id] = counter[id] + 1
+
+  socket.emit('spec vote', counter)
+})
+
+// var clicks = 0; //This is for the UpVote/Downvote Counters
+// var clicks2 = 0;
+// var clicks3 = 0;
+// var clicks4 = 0;
+
+// function counter1() {
+//   clicks += 1;
+//   var obj = {
+//     count1: ,
+//     count2: ,
+//     count3: ,
+//     count4: 
+//   }
+//   socket.emit("spec vote", clicks)
+// }
+
+// function counter2() {
+//   clicks2 += 1;
+//   socket.emit("spec vote", clicks2)
+// }
+
+/*function counter3() {
   clicks3 += 1;
-  console.log(clicks3)
- $('#counter3')
+ $('#counter3').html(clicks3);
 }
 function counter4() {
   clicks4 += 1;
-  console.log(clicks4)
- $('#counter4')
+ $('#counter4').html(clicks4);
+}*/
+
+//function for Socket.IO
+// function updateVote1(value) {
+//   $("#counter1").html(value);
+// }
+
+// function updateVote2(value) {
+//   $("#counter2").html(value);
+// }
+
+/*
+function updateVote3(value) {
+  $("#counter3").html(value);
 }
 
+function updateVote4(value) {
+  $("#counter4").html(value);
+}*/
+
+function seeCounter(obj) {
+  counter = obj
+  console.log(counter)
+}
 
 $.getJSON('/token/' + path, function (data) {
   identity = data.identity;
@@ -102,6 +142,35 @@ $.getJSON('/token/' + path, function (data) {
   socket.on('vid message', function(msg) {
     updateMessaging(msg.user, msg.message)
   })
+//Sockets for ThumbUP/ThumbDown
+  // socket.emit('spec vote', clicks)   
+  socket.on('spec vote', function(counter){
+    for(let key in counter) {
+      counter[key] = counter[key]
+      $('.' + key).html(counter[key])
+    }
+    console.log('counter after spec vote', counter)
+    seeCounter(counter)
+  })
+
+  // socket.on('spec vote', function(vote){
+  //   updateVote2(vote)
+  // })
+
+/*  socket.emit('spec vote', 'value')   
+  socket.on('spec vote', function(vote){
+    updateVote2(vote.value)
+  })
+
+  socket.emit('spec vote', 'value')   
+  socket.on('spec vote', function(vote){
+    updateVote3(vote.value)
+  })
+
+  socket.emit('spec vote', 'value')   
+  socket.on('spec vote', function(vote){
+    updateVote4(vote.value)
+  })  */
 
   // Create a Video Client and connect to Twilio
   videoClient = new Twilio.Video.Client(data.token);
@@ -148,6 +217,8 @@ $.getJSON('/token/' + path, function (data) {
   //   }
 
 });
+
+
 
 // Successfully connected!
 function roomJoined(room) {
@@ -205,7 +276,7 @@ function adjustVideo() {
   $("#local-media").animate({
       bottom: '-=35%',
       height: "-=350px",
-    }, 3000)
+    }, 1000)
   $("#waiting-overlay").css({display:"none"})
 }
 
@@ -235,6 +306,7 @@ function adjustBackVideo() {
 function updateMessaging(user, value) {
     $("#chat-window").append(`<div class=${identity===user ? 'chat-window-row-left' : 'chat-window-row-right'}><li class= ${identity===user ? 'user-style' : 'other-style'}>${user}: ${value}</li></div>`).scrollTop($("#chat-window")[0].scrollHeight);
 }
+
 
 
 // $("#chat-window").append(`<div class=${identity===user ? 'user-style':'other-style'} chat-window-row>${user}: <li class=chat-window-item>${value}</li>`).scrollTop($("#chat-window")[0].scrollHeight);
