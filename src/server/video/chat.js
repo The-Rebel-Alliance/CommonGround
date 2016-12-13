50d571e12c29b71b7fadfcc5351e0382b699b3fd
Michael Sweeney <jfmike@gmail.com>
2016-12-13 13:46:05 -0800

    added livetrack rooms

--- src/server/video/chat.js
+++ src/server/video/chat.js
@@ -1,10 +1,15 @@
-// import conn from '../lib/db'
+import conn from '../api/lib/db'
 
 export default function(vid) {
   var rooms = {}
+  var livetrackRooms = []
 
   vid.on('connection', function(socket){
     var room = ''
+
+    socket.on('livetrack', function(){
+      socket.join('livetrack')
+    })
 
     socket.on('join', function(roomName){
       socket.join(roomName)
@@ -15,19 +20,6 @@ export default function(vid) {
           specs:[]
         }
       }
-      // if (rooms[room].parts.length > 1) {
-      //   var sql = `
-      //     SELECT first_name, last_name, avatar
-      //     FROM profiles p
-      //     JOIN users u ON p.user_id = u.id
-      //     WHERE u.username = ?
-      //     || u.username = ?
-      //   `
-      //   conn.query()
-      //   vid.to('livetrack').emit({
-
-      //   })
-      // }
     })
 
     socket.on('participant connect', function(user){
@@ -38,6 +30,35 @@ export default function(vid) {
       vid.to(room).emit('participant connect', {
         username: user
       })
+      if (rooms[room].parts.length === 2) {
+        let sql = `
+          SELECT first_name, last_name, avatar
+          FROM profiles p
+          JOIN users u ON p.user_id = u.id
+          WHERE u.username = ?
+          OR u.username = ?
+        `
+        let participants = rooms[room].parts.map(part => part.username)
+        conn.query(sql, participants, function(err, results){
+          if (err) {
+            console.log(err)
+          }
+          livetrackRooms.push({
+            link: '/s/' + room,
+            user1: {
+              first_name: results[0].first_name,
+              last_name: results[0].last_name,
+              avatar: results[0].avatar
+            },
+            user2: {
+              first_name: results[1].first_name,
+              last_name: results[1].last_name,
+              avatar: results[1].avatar
+            }
+          })
+          vid.to('livetrack').emit('new room', livetrackRooms)
+        })
+      }
     })
 
     socket.on('spectator connect', function(){
@@ -68,8 +89,11 @@ export default function(vid) {
         })
 
         if (rooms[room].parts.length < 2) {
-          
-        }  
+          livetrackRooms = livetrackRooms.filter(function(livetrackroom){
+            return livetrackroom.link.indexOf(room) === -1
+          })
+          vid.to('livetrack').emit('new room', livetrackRooms)
+        }
       }
     })
 
